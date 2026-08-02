21c3addf84afa5b83ceb1d11fd473e26a45ae71d
Bartholomew Bassey <bartholomew.bassey@st.futminna.edu.ng>
2026-08-02 03:25:33 +0100

    updated the scripts

--- docker_user_data.sh
+++ docker_user_data.sh
@@ -1,7 +1,40 @@
 #!/bin/bash
-curl -fsSL https://get.docker.com/ | sh
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-yum install git -y
+set -euo pipeline
+echo "Updating System"
+sudo yum update -y 
+
+if command -v docker &>/dev/null 2>&1; then
+    echo "Installing -y docker"
+else
+    echo "Docker already installed..."
+fi
+
+echo "Starting Docker..."
+sudo systemctl enable --now docker
+
+echo "Installing Git...."
+if ! command -v git >/dev/null 2>&1; then
+    sudo yum install -y git
+fi
+
+echo "installing Docker Compose....."
+sudo mkdir -p /usr/local/lib/docker/cli-plugins
+sudo curl -SL \
+https://github.com/docker/compose/releases/latest/download/docker-compose-linux-$(uname -m) \
+-o /usr/local/lib/docker/cli-plugins/docker-compose
+
+sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+
+echo "Adding user to docker group..."
+sudo usermod -aG docker "$USER"
+
+echo
+echo "Installation complete!"
+echo
+
+docker --version
+docker compose version
+git --version
+
+echo
+echo "Log out and back in for docker group changes to take effect."
